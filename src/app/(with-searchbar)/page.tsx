f763f69f706e587d74ca73ad5ef7460811cb8761
kangsuyeong <ekdh16@naver.com>
2025-05-01 15:06:21 +0900

    feat: day 12완료

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -1,9 +1,61 @@
-import styles from "./page.module.css";
+import MovieItem from "@/components/movie-item";
+import style from "./page.module.css";
+import { MovieData } from "@/types";
+
+async function AllMovies() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie`,
+    { cache: "force-cache" }
+  );
+
+  if (!response.ok) {
+    return <div>오류가 발생했습니다....</div>;
+  }
+  const movies: MovieData[] = await response.json();
+  return (
+    <>
+      {movies.map((movie) => (
+        <MovieItem key={`all-${movie.id}`} {...movie} />
+      ))}
+    </>
+  );
+}
+
+async function RecoMovies() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie/random`,
+    { next: { revalidate: 3 } }
+  );
+  if (!response.ok) {
+    return <div>오류가 발생했습니다...</div>;
+  }
+
+  const moives: MovieData[] = await response.json();
+
+  return (
+    <>
+      {moives.map((movie) => (
+        <MovieItem key={`reco-${movie.id}`} {...movie} />
+      ))}
+    </>
+  );
+}
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <div>인덱스 페이지</div>
+    <div className={style.conatiner}>
+      <section>
+        <h3>지금 가장 추천하는 영화</h3>
+        <div className={style.reco_conatiner}>
+          <RecoMovies />
+        </div>
+      </section>
+      <section>
+        <h3>등록된 모든 영화</h3>
+        <div className={style.all_container}>
+          <AllMovies />
+        </div>
+      </section>
     </div>
   );
 }
--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -1,9 +1,53 @@
+import style from "./page.module.css";
+
 export default async function Page({
   params,
 }: {
-  params: Promise<{ id: string }>;
+  params: Promise<{ id: string | string[] }>;
 }) {
   const { id } = await params;
 
-  return <div>무비 : {id}</div>;
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie/${id}`,
+    { cache: "force-cache" }
+  );
+  if (!response.ok) {
+    return <div>오류가 발생했습니다...</div>;
+  }
+  const book = await response.json();
+
+  const {
+    title,
+    subTitle,
+    company,
+    runtime,
+    description,
+    posterImgUrl,
+    releaseDate,
+    genres,
+  } = book;
+
+  return (
+    <div className={style.container}>
+      <div
+        className={style.cover_img_container}
+        style={{ backgroundImage: `url('${posterImgUrl}')` }}
+      >
+        <img src={posterImgUrl} />
+      </div>
+      <div className={style.info_container}>
+        <div>
+          <h2>{title}</h2>
+          <div>
+            {releaseDate} / {genres.join(", ")} / {runtime}분
+          </div>
+          <div>{company}</div>
+        </div>
+        <div>
+          <div className={style.subTitle}>{subTitle}</div>
+          <div className={style.description}>{description}</div>
+        </div>
+      </div>
+    </div>
+  );
 }
