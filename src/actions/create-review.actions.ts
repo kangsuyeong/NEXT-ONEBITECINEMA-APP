04c5c68e59d121740288cb02cd4528844ae883d6
kangsuyeong <ekdh16@naver.com>
2025-05-23 20:53:36 +0900

    feat: day 17 완료

--- src/actions/create-review.actions.ts
+++ src/actions/create-review.actions.ts
@@ -1,6 +1,9 @@
 "use server";
 
-export async function createReviewAction(formData: FormData) {
+import { delay } from "@/util/delay";
+import { revalidateTag } from "next/cache";
+
+export async function createReviewAction(_: any, formData: FormData) {
   const movieId = formData.get("movieId")?.toString();
   const content = formData.get("content")?.toString();
   const author = formData.get("author")?.toString();
@@ -8,9 +11,13 @@ export async function createReviewAction(formData: FormData) {
   console.log(movieId, content, author);
 
   if (!movieId || !content || !author) {
-    return;
+    return {
+      status: false,
+      error: "리뷰 내용과 작성자를 입력해주세요.",
+    };
   }
 
+  await delay(2000);
   try {
     const response = await fetch(
       `${process.env.NEXT_PUBLIC_API_SERVER_URL}/review`,
@@ -19,8 +26,20 @@ export async function createReviewAction(formData: FormData) {
         body: JSON.stringify({ movieId, content, author }),
       }
     );
+
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
+    revalidateTag(`review-${movieId}`);
+    return {
+      status: true,
+      error: "",
+    };
   } catch (err) {
-    console.error(err);
-    return;
+    return {
+      status: false,
+      error: `리뷰 저장에 실패했습니다. ${err}`,
+    };
   }
 }
--- src/components/skeleton/movie-list-skeleton.tsx
+++ src/components/skeleton/movie-list-skeleton.tsx
@@ -1,5 +1,7 @@
 import MovieItemSkeleton from "@/components/skeleton/movie-item-skeleton";
 
 export default function MovieListSkeleton({ count }: { count: number }) {
-  return new Array(count).fill(0).map((_) => <MovieItemSkeleton />);
+  return new Array(count)
+    .fill(0)
+    .map((_, idx) => <MovieItemSkeleton key={`movie-list-skeleton-${idx}`} />);
 }
